d68f41869e0a857ab72e24ac649b68c58683d788
Stefan Guggisberg, PF42 <stefan.guggisberg.3@postfinance.ch>
2021-08-10 14:09:47 +0200

    first commit

---
+++ security/csrf/src/test/java/ch/guggisberg/stefan/csrf/CsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.guggisberg.stefan.csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
