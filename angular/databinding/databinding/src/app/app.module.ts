a107967b89e8c254a9bdafa0e742cb6ed5012b1d
Stefan Guggisberg <guggs2@bfh.ch>
2021-08-31 09:21:59 +0200

    Angular Databinding

--- angular/databinding/databinding/src/app/app.module.ts
+++ angular/databinding/databinding/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/databinding/databinding/src/app/other/other.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other',
+  templateUrl: './other.component.html'
+})
+export class OtherComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
